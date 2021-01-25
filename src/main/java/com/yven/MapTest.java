e59817bb738eb085727149a2b24c4f6f5e998755
yangwen <shaolong.yang@tuya.com>
2021-01-25 10:48:52 +0800

    feat:add

---
+++ src/main/java/com/yven/MapTest.java
@@ -0,0 +1,19 @@
+package com.yven;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description TODO
+ * @Author Edmond
+ * @Date 2021/01/05 22:09
+ * @Version 1.0
+ */
+public class MapTest {
+
+    public static void main(String[] args) {
+        Map map = new HashMap();
+
+        System.out.println(map.get(""));
+    }
+}
